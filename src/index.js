093122c87166410e36821b9d5d280a4ee9029a5a
wangqiaoxin <793021821@qq.com>
2020-04-13 22:34:31 +0800

    增加滚动方法

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 // eslint-disable-next-line no-console
 console.log('start')
 console.log('qx')
+
+function animationScrollToPoisition(top, dur) {
+  let reqId
+  let currentTop =
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop
+  const scrollLine = top - currentTop
+  const secondFps = 1000 / 60
+
+  // 所需帧率
+  const rafFps = Math.max(dur / secondFps, 1)
+  // 步长
+  const step = scrollLine / rafFps
+  function stepAnimation() {
+    if (Math.abs(currentTop - top) <= Math.abs(step)) {
+      window.cancelAnimationFrame(reqId)
+    } else {
+      currentTop += step
+      window.scrollTo({top: currentTop})
+      reqId = window.requestAnimationFrame(stepAnimation)
+    }
+  }
+  reqId = window.requestAnimationFrame(stepAnimation)
+}
+// this.clickTabTime = Date.now()
+const top = 500
+animationScrollToPoisition(top, 300)
