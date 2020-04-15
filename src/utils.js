8f13a994c5dc7eda30e8f22b42dde721b8a67162
hexin <hexin2014@xiaochuankeji.cn>
2020-04-15 11:11:45 +0800

    feat(src/utils.js): 增加获取字符串字节长度及按字节截断的方法

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param {number} top - 需要滚动到的目标 top 值
+ * @param {number} duration - 滚动到目标所需的时间 ms
+ */
 function animateScrollTo(top, duration) {
   let rafId
   let currentBodyScrollTop =
@@ -23,6 +28,29 @@ function animateScrollTo(top, duration) {
   window.requestAnimationFrame(animationStep)
 }
 
+// 获取字符长度、中文为2
+const getStrRealLength = str => {
+  if (str == null) return 0
+  // eslint-disable-next-line no-control-regex
+  return String(str).replace(/[^\x00-\xff]/g, '01').length
+}
+
+// 截取字符长度
+/**
+ *
+ * @param {string} str - 输入字符串
+ * @param {number} len  - 截取字符串长度
+ * @param {boolean} exact - 是否精确匹配字节长度
+ */
+const subStrRealLength = (str, len, exact) =>
+  exact
+    ? String(str)
+        .slice(0, len)
+        .replace(/([^x00-xff])/g, '$1a')
+        .slice(0, len)
+        .replace(/([^x00-xff])a/g, '$1')
+    : String(str).slice(0, len)
+
 const _ = ''
 
-export {_ as default, animateScrollTo}
+export {_ as default, animateScrollTo, getStrRealLength, subStrRealLength}
